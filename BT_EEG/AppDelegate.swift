6b29665ca8e7ad185852220ea37687f968652925
“Alizaybak” <dacker@brandeis.edu>
2015-10-20 07:11:50 -0400

    Initial Commit

---
+++ BT_EEG/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BT_EEG
+//
+//  Created by Daniel Acker on 10/20/15.
+//  Copyright (c) 2015 Daniel Acker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
